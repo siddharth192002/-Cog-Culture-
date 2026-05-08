a6e23fec59ca0979f11dced2dabda4e6f14e7813
Siddharth Tripathi <siddharthtri2002@gmail.com>
2026-05-08 17:07:15 +0530

    Cleanup: Remove test scripts and temporary files for production-ready repo

--- backend/test-pdf-2.cjs
+++
@@ -1,8 +0,0 @@
-const pdf = require('pdf-parse');
-console.log('Type of pdf.PDFParse:', typeof pdf.PDFParse);
-if (typeof pdf.PDFParse === 'function') {
-    console.log('PDFParse is a function');
-} else {
-    // Check if there is a default export that is a function
-    console.log('Type of pdf.default:', typeof pdf.default);
-}
--- backend/test-pdf.cjs
+++
@@ -1,3 +0,0 @@
-const pdf = require('pdf-parse');
-console.log('Type of pdf:', typeof pdf);
-console.log('PDF keys:', Object.keys(pdf));
